cc09e1280f797da357b14482cb01aadd4cd1d0a3
tanishqarora-nagarro <119845696+tanishqarora-nagarro@users.noreply.github.com>
2022-12-06 17:43:53 +0530

    Framework Setup and Demo App
    Set up the framework and a demo application

---
+++ sdk/iOS/DemoAppFlagright/DemoAppFlagright/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DemoAppFlagright
+//
+//  Created by tanishq on 06/12/22.
+//
+
+import UIKit
+import FrameworkFlagright
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let foo = DataCollection()
+        print(foo.name)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sdk/iOS/FrameworkFlagright/FrameworkFlagright/DataCollection.swift
@@ -0,0 +1,18 @@
+//
+//  DataCollection.swift
+//  FrameworkFlagright
+//
+//  Created by tanishq on 06/12/22.
+//
+
+import Foundation
+
+public class DataCollection {
+    
+    public init() {
+    }
+     
+    public let name = "foo"
+
+}
+
