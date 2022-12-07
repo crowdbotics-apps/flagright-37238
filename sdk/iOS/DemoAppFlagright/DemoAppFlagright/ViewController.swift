27c004a5ec7904e90caa061f51a2e39f0de414dd
tanishqarora-nagarro <119845696+tanishqarora-nagarro@users.noreply.github.com>
2022-12-07 13:21:39 +0530

    Added 10 features
    Added 10 features deviceID, lang, country, ram, os, maker, model, available memory, total memory + added UIDeviceExtension that returns accurate model number.

--- sdk/iOS/DemoAppFlagright/DemoAppFlagright/ViewController.swift
+++ sdk/iOS/DemoAppFlagright/DemoAppFlagright/ViewController.swift
@@ -12,8 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let foo = DataCollection()
-        print(foo.name)
+        let deviceData = DataCollection()
+        print(deviceData.name)
+        
+        print("The device ID is \(deviceData.deviceID)")
+        print("The device language is \(deviceData.language ?? "Error")")
+        print("The device country is \(deviceData.country ?? "Error")")
+        print("The available memory is \(deviceData.ram) GB")
+        deviceData.availableMemory()
+        deviceData.totalMemory()
+        print("The device is \(deviceData.isSimulator())")
+        print("The ios version is \(deviceData.systemVersion)")
+        print("The model is \(deviceData.modelName)")
+        print("The maker is \(deviceData.maker)")
+
+        
+
+        
         // Do any additional setup after loading the view.
     }
 
--- sdk/iOS/FrameworkFlagright/FrameworkFlagright/DataCollection.swift
+++ sdk/iOS/FrameworkFlagright/FrameworkFlagright/DataCollection.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 public class DataCollection {
     
@@ -13,6 +14,51 @@ public class DataCollection {
     }
      
     public let name = "foo"
-
+    public let deviceID = UIDevice.current.identifierForVendor!.uuidString
+    public let language = NSLocale.current.languageCode
+    public let country = NSLocale.current.regionCode
+    public let ram = ProcessInfo.processInfo.physicalMemory/(1024 * 1024 * 1024)
+    public let systemVersion = UIDevice.current.systemVersion
+    public let maker = "Apple"
+    public let modelName = UIDevice.modelName
+    
+    public func isSimulator() -> String{
+#if targetEnvironment(simulator)
+  return "Simulator"
+#else
+  return "Real Device"
+#endif
+    }
+ 
+    public func availableMemory(){
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory() as String)
+    do {
+        let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey])
+        if let capacity = values.volumeAvailableCapacity{
+            print("Available capacity: \(capacity)")
+        } else {
+            print("Capacity is unavailable")
+        }
+    } catch {
+        print("Error retrieving capacity: \(error.localizedDescription)")
+    }
+    }
+    
+    public func totalMemory(){
+    let fileURL = URL(fileURLWithPath: NSHomeDirectory() as String)
+    do {
+        let values = try fileURL.resourceValues(forKeys: [.volumeTotalCapacityKey])
+        if let capacity = values.volumeTotalCapacity {
+            print("Total capacity: \(capacity)")
+        } else {
+            print("Capacity is unavailable")
+        }
+    } catch {
+        print("Error retrieving capacity: \(error.localizedDescription)")
+    }
+    }
+    
+    
+    
 }
 
