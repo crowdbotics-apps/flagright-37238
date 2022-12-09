26bfd69265cb3680b83acf43e394c5569ab1d8a2
tanishqarora-nagarro <119845696+tanishqarora-nagarro@users.noreply.github.com>
2022-12-09 13:30:49 +0530

    Location and Contacts Partially added
    Added contacts and location features in the app itself and not the SDK just to check its working. Need to move some part to Framework/SDK.
    
    Also, added INFO plist changes for contacts

--- sdk/iOS/DemoAppFlagright/DemoAppFlagright/ViewController.swift
+++ sdk/iOS/DemoAppFlagright/DemoAppFlagright/ViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 import FrameworkFlagright
+import CoreLocation
+import Contacts
+import ContactsUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    let store = CNContactStore()
+    let keysToFetch = [CNContactGivenNameKey]
+    var contactArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let deviceData = DataCollection()
+
         
         print("The device ID is \(deviceData.deviceID)")
         print("The device language is \(deviceData.language ?? "Error")")
@@ -30,13 +39,64 @@ class ViewController: UIViewController {
         
         print("The jailbreak status is \(deviceData.jailBreakStatus())")
 
-
+        do {
+             try store.enumerateContacts(with: CNContactFetchRequest.init(keysToFetch: keysToFetch as [CNKeyDescriptor]), usingBlock: { (contact, pointer) -> Void in
+                // print("contact = ","\(contact.givenName)")
+                 self.contactArray.append(contact.givenName)
+             })
+         } catch {
+             print("something wrong happened in fetching contacts")
+         }
         
 
-        
+        print("Total number of contacts are \(self.contactArray.count)")
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+           
+           determineMyCurrentLocation()
+    }
+    
+    func determineMyCurrentLocation() {
+           locationManager = CLLocationManager()
+           locationManager.delegate = self
+           locationManager.desiredAccuracy = kCLLocationAccuracyBest
+           locationManager.requestAlwaysAuthorization()
+           
+           if CLLocationManager.locationServicesEnabled() {
 
+               locationManager.startUpdatingLocation()
+               //locationManager.startUpdatingHeading()
+           }
+       }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           let userLocation:CLLocation = locations[0] as CLLocation
+           
+           // Call stopUpdatingLocation() to stop listening for location updates,
+           // other wise this function will be called every time when user location changes.
+           
+          manager.stopUpdatingLocation()
+           
+           print("user latitude = \(userLocation.coordinate.latitude)")
+           print("user longitude = \(userLocation.coordinate.longitude)")
+        
+          switch locationManager.authorizationStatus {
+             case .notDetermined, .restricted, .denied:
+                 print("No access")
+             case .authorizedAlways, .authorizedWhenInUse:
+                 print("Access")
+             @unknown default:
+                 break
+         }
+       }
+       
+       func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+       {
+           print("Error")
+       }
 
 }
 
