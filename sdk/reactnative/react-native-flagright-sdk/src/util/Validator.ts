39dfe56c2c504800bf0793e391628faedbe86902
tarun-crowdbotics <tarun.joshi@crowdbotics.com>
2023-01-24 13:22:04 +0530

    Added region enum and validator

---
+++ sdk/reactnative/react-native-flagright-sdk/src/util/Validator.ts
@@ -0,0 +1,32 @@
+import { Region } from 'src/types/Region';
+
+function validateString(str: string) {
+  if (str && str.trim().length > 0) {
+    return true;
+  }
+  return false;
+}
+
+export function validateAPIKey(apiKey: string) {
+  return validateString(apiKey);
+}
+
+export function validateUserId(userId: string) {
+  return validateString(userId);
+}
+
+export function validateRegion(region: string) {
+  if (region) {
+    let validated = false;
+    if (
+      region === Region.ASIA1 ||
+      region === Region.ASIA2 ||
+      region === Region.EU1 ||
+      region === Region.EU2 ||
+      region === Region.US1
+    )
+      validated = true;
+    return validated;
+  }
+  return false;
+}
